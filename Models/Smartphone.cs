5322f5d4991b398ee8ec01ac29d852cddc4f1928
jrd-learn <105658679+jrd-learn@users.noreply.github.com>
2023-03-03 08:43:19 -0300

    Projeto clonado

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -1,14 +1,23 @@
+using System;
 namespace DesafioPOO.Models
 {
     public abstract class Smartphone
     {
-        public string Numero { get; set; }
         // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        // *IMPLEMENTADO*
+        public string Numero { get; set; }
+        public string Modelo { get; set; }
+        public string Imei { get; set; }
+        public int Memoria { get; set; }
 
-        public Smartphone(string numero)
+        // TODO: Passar os parâmetros do construtor para as propriedades
+        // *IMPLEMENTADO*
+        public Smartphone(string numero, string modelo, string imei, int memoria)
         {
             Numero = numero;
-            // TODO: Passar os parâmetros do construtor para as propriedades
+            Modelo = modelo;
+            Imei = imei;
+            Memoria = memoria;
         }
 
         public void Ligar()
--- Program.cs
+++ Program.cs
@@ -1,3 +1,11 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        
+    }
+}
